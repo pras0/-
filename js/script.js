305bf34eb9cd897bcdf19054aa5ada6b5f2838d9
Prashant Shekhar <prashantshekharniki6@gmail.com>
2019-11-30 23:18:21 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.querySelector('.fa-times').addEventListener('click', function () {
+    document.querySelector('.fa-times').classList.toggle('close-menu');
+    document.querySelector('.fa-bars').classList.toggle('open-menu');
+    document.querySelector('.navigation').classList.toggle('open-menu');
+});
